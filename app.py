110248415fced24c0583cb87355c6e0b6eb6d159
serjaalo <serjaalo@gmail.com>
2026-04-30 14:13:59 +0900

    done

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ with open('awards.json', encoding='utf-8') as f:
     awards = json.load(f)
 
 # TODO: Добавить код для чтения лауреатов из файла
+with open('laureats.json', encoding='utf-8') as f:
+    laureats = json.load(f)
+
+laureat_id = {v['id']: k for k, v in enumerate(laureats)}
 
 
 @app.route("/api/v1/awards/")
@@ -39,6 +43,26 @@ def award_object(pk):
 
 
 # TODO: Добавить код для получения списка лауреатов
+@app.route("/v2/laureats/")
+def laureats_list():
+    try:
+        p = int(request.args.get('p', 0))
+        if p < 0:
+            raise ValueError
+    except ValueError:
+        return abort(400)
+    page = laureats[p * 50:(p + 1)*50]
+    return jsonify({
+        'page': p,
+        'count_on_page': PAGE_SIZE,
+        'total': len(laureats),
+        'items': page,
+    })
 
 
 # TODO: Добавить код для получения лауреата по индексу
+@app.route("/v2/laureats/<id>")
+def laureat_object(id):
+    if id in laureat_id:
+        return jsonify(laureats[laureat_id[id]])
+    abort(404)
